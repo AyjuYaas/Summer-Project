4d532ed7eee8b7f8215dd324689cf3d266c0f964
SAYUJYA SATYAL <sayujya57@gmail.com>
2025-05-07 17:37:23 +0545

    Add laxy & suspense

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,40 +1,40 @@
-import React, { useEffect, useState } from "react";
+import React, { lazy, Suspense, useEffect, useState } from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
 import ReactLoading from "react-loading";
 
 // ======== Nav Routes =============
 import Navbar from "./components/Navbar";
-import Home from "./pages/Home";
-import About from "./pages/About";
-import Contact from "./pages/Contact";
-import TherapistList from "./pages/TherapistList";
+const Home = lazy(() => import("./pages/Home"));
+const About = lazy(() => import("./pages/About"));
+const Contact = lazy(() => import("./pages/Contact"));
+const TherapistList = lazy(() => import("./pages/TherapistList"));
+// ======== General Pages =============
+const GetStarted = lazy(() => import("./pages/GetStarted"));
+const Chat = lazy(() => import("./pages/Chat"));
+const VideoCall = lazy(() => import("./pages/VideoCall"));
 
 // ======== User Routes =============
-import Login from "./pages/UserLogin";
-import UserDashboard from "./pages/UserDashboard";
-
-import FindTherapist from "./pages/FindTherapist";
+const Login = lazy(() => import("./pages/UserLogin"));
+const UserDashboard = lazy(() => import("./pages/UserDashboard"));
+const FindTherapist = lazy(() => import("./pages/FindTherapist"));
 
 // ======== Therapist Routes =============
-import TherapistLogin from "./pages/TherapistLogin";
-import TherapistDashboard from "./pages/TherapistDashboard";
+const TherapistLogin = lazy(() => import("./pages/TherapistLogin"));
+const TherapistDashboard = lazy(() => import("./pages/TherapistDashboard"));
 
 // ======== Profile Update Routes =============
-import UpdateProfile from "./pages/UpdateProfile";
+const UpdateProfile = lazy(() => import("./pages/UpdateProfile"));
 
 // ======== Default Routes =============
-import Admin from "./pages/Admin";
-import NotFound from "./pages/NotFound";
+const Admin = lazy(() => import("./pages/Admin"));
+const NotFound = lazy(() => import("./pages/NotFound"));
 
 // ======== Auth and Toast =============
 import { useAuthStore } from "./store/useAuthStore";
 import { Toaster } from "react-hot-toast";
-import Chat from "./pages/Chat";
-import GetStarted from "./pages/GetStarted";
 import { useMatchStore } from "./store/useMatchStore";
 import { useMessageStore } from "./store/useMessageStore";
 import Test from "./pages/Test";
-import VideoCall from "./pages/VideoCall";
 
 export default function App(): React.ReactElement {
   const { checkAuth, authUser, authType, checkingAuth } = useAuthStore();
@@ -118,127 +118,137 @@ export default function App(): React.ReactElement {
       {!hideNavbar && (
         <Navbar openNavbar={openNavbar} setOpenNavbar={setOpenNavbar} />
       )}
+      <Suspense
+        fallback={
+          <div className="min-h-screen flex flex-col gap-2 justify-center items-center z-40 w-full">
+            <ReactLoading type="spin" color="#303b36" />
+            <h1 className="text-xl text-[#303b36] font-extrabold">
+              Loading...
+            </h1>
+          </div>
+        }
+      >
+        <Routes>
+          {/* Default Nav Routes  */}
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
 
-      <Routes>
-        {/* Default Nav Routes  */}
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
+          <Route path="/therapist-list" element={<TherapistList />} />
+          <Route path="/contact" element={<Contact />} />
 
-        <Route path="/therapist-list" element={<TherapistList />} />
-        <Route path="/contact" element={<Contact />} />
+          {/* Login Route */}
+          <Route
+            path="/user/login"
+            element={
+              !authUser ? (
+                <Login />
+              ) : authType === "user" ? (
+                <Navigate to="/user/home" />
+              ) : authType === "therapist" ? (
+                <Navigate to="/therapist/home" />
+              ) : (
+                <Login />
+              )
+            }
+          />
 
-        {/* Login Route */}
-        <Route
-          path="/user/login"
-          element={
-            !authUser ? (
-              <Login />
-            ) : authType === "user" ? (
-              <Navigate to="/user/home" />
-            ) : authType === "therapist" ? (
-              <Navigate to="/therapist/home" />
-            ) : (
-              <Login />
-            )
-          }
-        />
+          <Route
+            path="/get-started"
+            element={
+              !authUser || authType === "therapist" ? (
+                <GetStarted />
+              ) : (
+                <Navigate to="/user/find-therapist" />
+              )
+            }
+          />
 
-        <Route
-          path="/get-started"
-          element={
-            !authUser || authType === "therapist" ? (
-              <GetStarted />
-            ) : (
-              <Navigate to="/user/find-therapist" />
-            )
-          }
-        />
+          {/* User Routes */}
+          <Route
+            path="/user/home"
+            element={
+              authUser && authType === "user" ? (
+                <UserDashboard />
+              ) : (
+                <Navigate to="/user/login" />
+              )
+            }
+          />
+          <Route
+            path="/user/update-profile"
+            element={
+              authUser && authType === "user" ? (
+                <UpdateProfile />
+              ) : (
+                <Navigate to="/user/login" />
+              )
+            }
+          />
+          {/* <Route path="/user/find-therapist" element={<FindTherapist />} /> */}
+          <Route
+            path="/user/find-therapist"
+            element={
+              authUser && authType === "user" ? (
+                <FindTherapist />
+              ) : (
+                <Navigate to="/get-started" />
+              )
+            }
+          />
 
-        {/* User Routes */}
-        <Route
-          path="/user/home"
-          element={
-            authUser && authType === "user" ? (
-              <UserDashboard />
-            ) : (
-              <Navigate to="/user/login" />
-            )
-          }
-        />
-        <Route
-          path="/user/update-profile"
-          element={
-            authUser && authType === "user" ? (
-              <UpdateProfile />
-            ) : (
-              <Navigate to="/user/login" />
-            )
-          }
-        />
-        {/* <Route path="/user/find-therapist" element={<FindTherapist />} /> */}
-        <Route
-          path="/user/find-therapist"
-          element={
-            authUser && authType === "user" ? (
-              <FindTherapist />
-            ) : (
-              <Navigate to="/get-started" />
-            )
-          }
-        />
+          {/* Therapist Routes */}
+          <Route
+            path="/therapist/login"
+            element={
+              !authUser ? (
+                <TherapistLogin />
+              ) : authUser && authType === "therapist" ? (
+                <Navigate to="/therapist/home" />
+              ) : (
+                <Navigate to="/user/home" />
+              )
+            }
+          />
+          <Route
+            path="/therapist/home"
+            element={
+              authUser && authType === "therapist" ? (
+                <TherapistDashboard />
+              ) : (
+                <Navigate to="/therapist/login" />
+              )
+            }
+          />
+          <Route
+            path="/therapist/update-profile"
+            element={
+              authUser && authType === "therapist" ? (
+                <UpdateProfile />
+              ) : (
+                <Navigate to="/therapist/login" />
+              )
+            }
+          />
 
-        {/* Therapist Routes */}
-        <Route
-          path="/therapist/login"
-          element={
-            !authUser ? (
-              <TherapistLogin />
-            ) : authUser && authType === "therapist" ? (
-              <Navigate to="/therapist/home" />
-            ) : (
-              <Navigate to="/user/home" />
-            )
-          }
-        />
-        <Route
-          path="/therapist/home"
-          element={
-            authUser && authType === "therapist" ? (
-              <TherapistDashboard />
-            ) : (
-              <Navigate to="/therapist/login" />
-            )
-          }
-        />
-        <Route
-          path="/therapist/update-profile"
-          element={
-            authUser && authType === "therapist" ? (
-              <UpdateProfile />
-            ) : (
-              <Navigate to="/therapist/login" />
-            )
-          }
-        />
+          {/* Chat Page */}
+          <Route
+            path="chat/:id"
+            element={authUser ? <Chat /> : <Navigate to="/user/login" />}
+          />
 
-        {/* Chat Page */}
-        <Route
-          path="chat/:id"
-          element={authUser ? <Chat /> : <Navigate to="/user/login" />}
-        />
+          {/* Admin Route */}
+          <Route path="/admin" element={<Admin />} />
 
-        {/* Admin Route */}
-        <Route path="/admin" element={<Admin />} />
+          <Route path="/test" element={<Test />} />
+          <Route
+            path="/video-call/:receiverId"
+            element={authUser ? <VideoCall /> : <Navigate to="/user/login" />}
+          />
 
-        <Route path="/test" element={<Test />} />
-        <Route
-          path="/video-call/:receiverId"
-          element={authUser ? <VideoCall /> : <Navigate to="/user/login" />}
-        />
-
-        {/* Wrong Route */}
-        <Route path="*" element={<NotFound />} />
-      </Routes>
+          {/* Wrong Route */}
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Suspense>
     </div>
   );
 }
